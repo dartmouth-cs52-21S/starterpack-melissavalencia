0c71ea36e489767aefa012e4173f5541eb6b820e
Melissa Valencia <melissa.valencia.23@dartmouth.edu>
2021-04-15 18:31:31 -0700

    done with sa2

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('starting up!');
+// const $ = require('jquery');
+// $('#main').html('Here we go!');
+
+// change require to es6 import style
+import $ from 'jquery';
+import './style.scss';
+
+let sec = 0;
+
+setInterval(() => { $('#main').html(`You have been on this page for ${sec} seconds.`); sec += 1; }, 1000);
