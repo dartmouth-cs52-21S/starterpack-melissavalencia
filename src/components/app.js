1bab4ac15fcc656c55e8339c969e92f4216245de
Melissa Valencia <melissa.valencia.23@dartmouth.edu>
2021-04-30 10:50:51 -0700

    part1 redux

---
+++ src/components/app.js
@@ -0,0 +1,54 @@
+import React from 'react';
+// import ReactDOM from 'react-dom';
+import {
+  BrowserRouter as Router, Route, NavLink, Switch,
+} from 'react-router-dom';
+import '../style.scss';
+import Counter from './counter';
+import Controls from './controls';
+
+const About = (props) => {
+  return <div> All there is to know about me </div>;
+};
+const Welcome = (props) => {
+  return <div>Welcome</div>;
+};
+
+const Test = (props) => {
+  return <div> ID: {props.match.params.id} </div>;
+};
+
+const Nav = (props) => {
+  return (
+    <nav>
+      <ul>
+        <li><NavLink to="/" exact>Home</NavLink></li>
+        <li><NavLink to="/about">About</NavLink></li>
+        <li><NavLink to="/test/id1">test id1</NavLink></li>
+        <li><NavLink to="/test/id2">test id2</NavLink></li>
+      </ul>
+    </nav>
+  );
+};
+
+const FallBack = (props) => {
+  return <div>URL Not Found</div>;
+};
+// const App = () => <div className="test">All the REACT are belong to us!</div>;
+
+const App = (props) => {
+  return (
+    <Router>
+      <Nav />
+      <Switch>
+        <Controls exact path="/" component={Welcome} />
+        <Counter path="/about" component={About} />
+        <Route exact path="/test/:id" component={Test} />
+        <Route component={FallBack} />
+      </Switch>
+    </Router>
+  );
+};
+
+export default App;
+// ReactDOM.render(<App />, document.getElementById('main'));
